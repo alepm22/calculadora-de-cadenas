f430b9deb89e95cecae144ebc2fb753fd2365c35
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-08 22:26:15 -0400

    Refactorizacion de la funcion calcular-cadena

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,4 +1,9 @@
 function calcularCadena(cadena) {
+  const CERO=0
+  if (cadena.length!=0)
+    {
+      return parseInt(cadena[0]);
+    }
   return 0
 }
 
--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -1,7 +1,10 @@
-import calcularCadena from "./calculadoraCadenas.js"
+import calcularCadena from "./calculadoraCadenas.js";
 
 describe("Calculadora de cadenas", () => {
   it("Debería retornar 0 cuando la cadena está vacía", () => {
     expect(calcularCadena("")).toEqual(0);
+  });
+  it("Debería retornar 3 ya que la cadena contiene el número 3", () => {
+    expect(calcularCadena("3")).toEqual(3);
   });
 });
