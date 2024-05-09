1b1ee5900507c0f559782d5bcd20b0e60429205f
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-08 22:47:09 -0400

    Caso de prueba donde se envian dos elementos y se devuelve la suma de los elementos

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,9 +1,13 @@
 function calcularCadena(cadena) {
   const CERO=0
-  if (cadena.length!=0)
+  const longitudCadena=cadena.length
+  if (longitudCadena==1)
     {
       return parseInt(cadena[0]);
     }
+  if(longitudCadena==3){
+    return parseInt(cadena[0])+parseInt(cadena[2])
+  }
   return 0
 }
 
--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -4,7 +4,10 @@ describe("Calculadora de cadenas", () => {
   it("Debería retornar 0 cuando la cadena está vacía", () => {
     expect(calcularCadena("")).toEqual(0);
   });
-  it("Debería retornar 3 ya que la cadena contiene el número 3", () => {
+  it("Debería retornar 3 para una cadena con un solo número", () => {
     expect(calcularCadena("3")).toEqual(3);
+  });
+  it("Debería devolver 5 porque la cadena contiene dos elementos", () => {
+    expect(calcularCadena("3,2")).toEqual(5);
   });
 });
