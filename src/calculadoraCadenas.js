f28f4aa3874dca20f55e604bda83df2e29717064
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-08 23:28:22 -0400

    se refactorizo la funcion en calcular cadena

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,33 +1,36 @@
 function calcularCadena(cadena) {
   const CERO=0
   let sumaTotal=CERO
-  const longitudCadena=cadena.length
+  const TAMANIOCADENA=cadena.length
   const SEPARADORUSUARIO=cadena[3]
-  let cadenaEstaVacia=longitudCadena!=0
+  let cadenaEstaVacia=TAMANIOCADENA!=0
   let cadenaNumeros=[]
   if (cadenaEstaVacia)
     {
-      for (let iteradorCadena=0;iteradorCadena<longitudCadena;iteradorCadena++)
+      for (let iteradorCadena=0;iteradorCadena<TAMANIOCADENA;iteradorCadena++)
         {
           let caracter=cadena[iteradorCadena]
           let siguienteCaracter=cadena[iteradorCadena+1]
-          let caracterEsUnNumeroSeparadoPorComa=!isNaN(caracter) && siguienteCaracter=="," && iteradorCadena!=longitudCadena-1
-          let caracterEsElUltimoNumero=iteradorCadena==longitudCadena-1
-          let caracterEsUnNumeroSeparadoPorGuion=!isNaN(caracter) && siguienteCaracter=="-" && iteradorCadena!=longitudCadena-1
-          let caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario=!isNaN(caracter) && siguienteCaracter==SEPARADORUSUARIO && iteradorCadena!=longitudCadena-1
+          let caracterEsUnNumeroSeparadoPorComa=!isNaN(caracter) && siguienteCaracter=="," && iteradorCadena!=TAMANIOCADENA-1
+          let caracterEsElUltimoNumero=iteradorCadena==TAMANIOCADENA-1
+          let caracterEsUnNumeroSeparadoPorGuion=!isNaN(caracter) && siguienteCaracter=="-" && iteradorCadena!=TAMANIOCADENA-1
+          let caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario=!isNaN(caracter) && siguienteCaracter==SEPARADORUSUARIO && iteradorCadena!=TAMANIOCADENA-1
           if (!isNaN(caracter)){
             cadenaNumeros.push(caracter)
           }
-          if((caracterEsUnNumeroSeparadoPorComa || caracterEsElUltimoNumero || caracterEsUnNumeroSeparadoPorGuion || caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario))
+          if((caracterEsUnNumeroSeparadoPorComa || caracterEsElUltimoNumero || caracterEsUnNumeroSeparadoPorGuion || caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario) && (parseInt(cadenaNumeros.join(''))<=1000))
             {
-              let numeroASumarEsMenorIgual1000=parseInt(cadenaNumeros.join(''))<=1000
-              if(numeroASumarEsMenorIgual1000)
-                {
+              
                   sumaTotal=sumaTotal+parseInt(cadenaNumeros.join(''))
 
-                }
+                
               cadenaNumeros=[]
             }
+            if((parseInt(cadenaNumeros.join(''))>1000))
+              {
+                cadenaNumeros=[]
+              }
+
         }
     }
   return sumaTotal
