2642f6b899685b854c222e133e17e0dc18a24d40
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-16 05:45:15 -0400

    se arreglo el ultimo caso de //[*][%] 1*2%3,7-9 donde deria retornar 22

--- src/calculadoraCadenas.js
+++ src/calculadoraCadenas.js
@@ -1,39 +1,67 @@
+function obtenerSeparadoresPersonalizados(cadena) {
+  const regexSeparadores = /\[(.*?)\]/g;
+  let separadoresPersonalizados = [];
+  let resultadoRegex;
+  while ((resultadoRegex = regexSeparadores.exec(cadena)) !== null) {
+    separadoresPersonalizados.push(resultadoRegex[1]);
+  }
+  return separadoresPersonalizados;
+}
+
+function esNumeroValido(cadenaNumeros) {
+  const numero = parseInt(cadenaNumeros.join(""));
+  return numero <= 1000;
+}
+
+function esSeparadorValido(caracterActual, siguienteCaracter, cadena, indice, separadoresPersonalizados) {
+  const TAMANIOCADENA = cadena.length;
+  return (
+    (!isNaN(caracterActual) &&
+      (siguienteCaracter === "," ||
+       siguienteCaracter === "-" ||
+       separadoresPersonalizados.some(separador => 
+         cadena.substring(indice + 1, indice + 1 + separador.length) === separador)
+      ) &&
+      indice !== TAMANIOCADENA - 1) ||
+    indice === TAMANIOCADENA - 1
+  );
+}
+
 function calcularCadena(cadena) {
-  const CERO=0
-  let sumaTotal=CERO
-  const TAMANIOCADENA=cadena.length
-  const SEPARADORUSUARIO=cadena[3]
-  let cadenaEstaVacia=TAMANIOCADENA!=0
-  let cadenaNumeros=[]
-  if (cadenaEstaVacia)
-    {
-      for (let iteradorCadena=0;iteradorCadena<TAMANIOCADENA;iteradorCadena++)
-        {
-          let caracter=cadena[iteradorCadena]
-          let siguienteCaracter=cadena[iteradorCadena+1]
-          let caracterEsUnNumeroSeparadoPorComa=!isNaN(caracter) && siguienteCaracter=="," && iteradorCadena!=TAMANIOCADENA-1
-          let caracterEsElUltimoNumero=iteradorCadena==TAMANIOCADENA-1
-          let caracterEsUnNumeroSeparadoPorGuion=!isNaN(caracter) && siguienteCaracter=="-" && iteradorCadena!=TAMANIOCADENA-1
-          let caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario=!isNaN(caracter) && siguienteCaracter==SEPARADORUSUARIO && iteradorCadena!=TAMANIOCADENA-1
-          if (!isNaN(caracter)){
-            cadenaNumeros.push(caracter)
-          }
-          if((caracterEsUnNumeroSeparadoPorComa || caracterEsElUltimoNumero || caracterEsUnNumeroSeparadoPorGuion || caracterEsUnNumeroSeparadoPorCaracterEspecificadoPorUsuario) && (parseInt(cadenaNumeros.join(''))<=1000))
-            {
-              
-                  sumaTotal=sumaTotal+parseInt(cadenaNumeros.join(''))
+  const CERO = 0;
+  let sumaTotal = CERO;
+  const TAMANIOCADENA = cadena.length;
 
-                
-              cadenaNumeros=[]
-            }
-            if((parseInt(cadenaNumeros.join(''))>1000))
-              {
-                cadenaNumeros=[]
-              }
+  if (TAMANIOCADENA === 0) return sumaTotal;
 
-        }
+  let cadenaNumeros = [];
+  const separadoresPersonalizados = obtenerSeparadoresPersonalizados(cadena);
+  const SEPARADORUSUARIO = separadoresPersonalizados.length > 0 ? separadoresPersonalizados[0] : cadena[3];
+  let cadenaEstaVacia = TAMANIOCADENA !== 0;
+
+  if (cadenaEstaVacia) {
+    for (let iteradorCadena = 0; iteradorCadena < TAMANIOCADENA; iteradorCadena++) {
+      let caracter = cadena[iteradorCadena];
+      let siguienteCaracter = cadena[iteradorCadena + 1];
+
+      if (!isNaN(caracter)) {
+        cadenaNumeros.push(caracter);
+      }
+
+      if (esSeparadorValido(caracter, siguienteCaracter, cadena, iteradorCadena, separadoresPersonalizados) &&
+          esNumeroValido(cadenaNumeros)) {
+        sumaTotal += parseInt(cadenaNumeros.join(''));
+        cadenaNumeros = [];
+      }
+
+      if (esSeparadorValido(caracter, siguienteCaracter, cadena, iteradorCadena, separadoresPersonalizados) &&
+          !esNumeroValido(cadenaNumeros)) {
+        cadenaNumeros = [];
+      }
     }
-  return sumaTotal
+  }
+
+  return sumaTotal;
 }
 
 export default calcularCadena;
